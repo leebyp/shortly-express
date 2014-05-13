c402ce4c9be4453842e578503d7967682a0b1fe1
Hack Reactor Students <leebyp@gmail.com>
2014-05-12 17:12:21 -0700

    Hash password before storing

--- app/models/user.js
+++ app/models/user.js
@@ -3,6 +3,25 @@ var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
 var User = db.Model.extend({
+  tableName: 'users',
+  hasTimestamps: true,
+  initialize: function(){
+    this.on('creating', function(model, attrs, options) {
+      /*bcrypt.hash(model.get('password'), null, null, function(err, hash) {
+        if(err){
+          console.error('error hashing password');
+        } else {
+          console.log(hash);
+          console.log(model);
+          model.set('password', hash);
+          console.log(model.get('password'));
+          console.log(model);
+          console.log('password has been set to', hash)
+        }
+      });*/
+      model.set('password', bcrypt.hashSync(model.get('password')));
+    });
+  }
 });
 
 module.exports = User;
